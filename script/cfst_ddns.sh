80566c70e2be66c54503e192c1d050ba91d90f43
yyyytawa <116234899+yyyytawa@users.noreply.github.com>
2024-10-07 11:58:18 +0800

    新增 cfst_ddns.sh支持多子域名(仅限同一类型,同一个区域ID下的)没有更新Windows的bat脚本 新增 cfst_ddns.conf的示例配置文件

--- script/cfst_ddns.sh
+++ script/cfst_ddns.sh
@@ -3,10 +3,13 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 # --------------------------------------------------------------
 #	项目: CloudflareSpeedTest 自动更新域名解析记录
-#	版本: 1.0.5
-#	作者: XIU2
+#	版本: 1.0.6
+#	作者: XIU2(大部分代码) & yyyyt(少部分)
 #	项目: https://github.com/XIU2/CloudflareSpeedTest
 # --------------------------------------------------------------
+# 如果你想要指定自定义配置文件的话请将下面的"cfst_ddns.conf"换成对应的文件名
+# 多子域名的已经默认注释,如果你想更新多子域名的话请在配置文件指定NAME2和DNS_RECORDS_ID2并取消掉对应的注释(_READ的和下面curl的)
+# 如果你想要v4 v6都优选的话请将脚本文件复制一份并修改为指定配置文件~~(其实一个脚本也是可以的,但是我懒得改2333)~~,配置文件填上对应的内容 测速时候分别去运行两个脚本
 
 _READ() {
 	[[ ! -e "cfst_ddns.conf" ]] && echo -e "[错误] 配置文件不存在 [cfst_ddns.conf] !" && exit 1
@@ -17,6 +20,8 @@ _READ() {
 	[[ -z "${ZONE_ID}" ]] && echo -e "[错误] 缺少配置项 [ZONE_ID] !" && exit 1
 	DNS_RECORDS_ID=$(echo "${CONFIG}"|grep 'DNS_RECORDS_ID='|awk -F '=' '{print $NF}')
 	[[ -z "${DNS_RECORDS_ID}" ]] && echo -e "[错误] 缺少配置项 [DNS_RECORDS_ID] !" && exit 1
+#	DNS_RECORDS_ID2=$(echo "${CONFIG}"|grep 'DNS_RECORDS_ID2='|awk -F '=' '{print $NF}')
+#	[[ -z "${DNS_RECORDS_ID2}" ]] && echo -e "[错误] 缺少配置项 [DNS_RECORDS_ID2] !" && exit 1
 	KEY=$(echo "${CONFIG}"|grep 'KEY='|awk -F '=' '{print $NF}')
 	[[ -z "${KEY}" ]] && echo -e "[错误] 缺少配置项 [KEY] !" && exit 1
 	EMAIL=$(echo "${CONFIG}"|grep 'EMAIL='|awk -F '=' '{print $NF}')
@@ -25,20 +30,27 @@ _READ() {
 	[[ -z "${TYPE}" ]] && echo -e "[错误] 缺少配置项 [TYPE] !" && exit 1
 	NAME=$(echo "${CONFIG}"|grep 'NAME='|awk -F '=' '{print $NF}')
 	[[ -z "${NAME}" ]] && echo -e "[错误] 缺少配置项 [NAME] !" && exit 1
+#	NAME2=$(echo "${CONFIG}"|grep 'NAME2='|awk -F '=' '{print $NF}')
+#	[[ -z "${NAME2}" ]] && echo -e "[错误] 缺少配置项 [NAME2] !" && exit 1
 	TTL=$(echo "${CONFIG}"|grep 'TTL='|awk -F '=' '{print $NF}')
 	[[ -z "${TTL}" ]] && echo -e "[错误] 缺少配置项 [TTL] !" && exit 1
 	PROXIED=$(echo "${CONFIG}"|grep 'PROXIED='|awk -F '=' '{print $NF}')
 	[[ -z "${PROXIED}" ]] && echo -e "[错误] 缺少配置项 [PROXIED] !" && exit 1
+    RESULT=$(echo "${CONFIG}"|grep 'RESULT='|awk -F '=' '{print $NF}')
+    [[ -z "${RESULT}" ]] && echo -e "[错误] 缺少配置项 [RESULT] !" && exit 1
 }
 
 _UPDATE() {
-	# 这里可以自己添加、修改 CloudflareST 的运行参数
-	./CloudflareST -o "result_ddns.txt"
+	# 执行前删除原有的测速结果
+	rm -rf "${RESULT}"
+
+	# 这里可以自己添加、修改 CloudflareST 的运行参数.如果你想要测速非cfcdn的节点/IPv6的话请指定-f参数
+	./CloudflareST -o "${RESULT}" -url https://download.parallels.com/desktop/v17/17.1.1-51537/ParallelsDesktop-17.1.1-51537.dmg
 
 	# 判断结果文件是否存在，如果不存在说明结果为 0
-	[[ ! -e "result_ddns.txt" ]] && echo "CloudflareST 测速结果 IP 数量为 0，跳过下面步骤..." && exit 0
+	[[ ! -e "${RESULT}" ]] && echo "CloudflareST 测速结果 IP 数量为 0，跳过下面步骤..." && exit 0
 
-	CONTENT=$(sed -n "2,1p" result_ddns.txt | awk -F, '{print $1}')
+	CONTENT=$(sed -n "2,1p" "${RESULT}" | awk -F, '{print $1}')
 	if [[ -z "${CONTENT}" ]]; then
 		echo "CloudflareST 测速结果 IP 数量为 0，跳过下面步骤..."
 		exit 0
@@ -51,12 +63,21 @@ _UPDATE() {
 			-H "X-Auth-Key: ${KEY}" \
 			-H "Content-Type: application/json" \
 			--data "{\"type\":\"${TYPE}\",\"name\":\"${NAME}\",\"content\":\"${CONTENT}\",\"ttl\":${TTL},\"proxied\":${PROXIED}}"
+#		curl -X PUT "https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records/${DNS_RECORDS_ID2}" \
+#			-H "X-Auth-Email: ${EMAIL}" \
+#			-H "X-Auth-Key: ${KEY}" \
+#			-H "Content-Type: application/json" \
+#			--data "{\"type\":\"${TYPE}\",\"name\":\"${NAME2}\",\"content\":\"${CONTENT}\",\"ttl\":${TTL},\"proxied\":${PROXIED}}"
 	else
 		# API 令牌方式（自定义权限）
 		curl -X PUT "https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records/${DNS_RECORDS_ID}" \
 			-H "Authorization: Bearer ${KEY}" \
 			-H "Content-Type: application/json" \
 			--data "{\"type\":\"${TYPE}\",\"name\":\"${NAME}\",\"content\":\"${CONTENT}\",\"ttl\":${TTL},\"proxied\":${PROXIED}}"
+#		curl -X PUT "https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records/${DNS_RECORDS_ID2}" \
+#			-H "Authorization: Bearer ${KEY}" \
+#			-H "Content-Type: application/json" \
+#			--data "{\"type\":\"${TYPE}\",\"name\":\"${NAME2}\",\"content\":\"${CONTENT}\",\"ttl\":${TTL},\"proxied\":${PROXIED}}"
 	fi
 }
 
